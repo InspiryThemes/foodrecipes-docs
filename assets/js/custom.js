ae96dc0f749e379a9b21deb981d429274e3948f1
sungraizfaryad <sungraiz@inspirythemes.com>
2018-12-03 17:04:21 +0500

    Doc JS Improved

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,12 +10,27 @@ $(document).ready(function(){
     nav.find('.sub-menu-item').click(function(){
         nav.find('.sub-menu-item').removeClass('active');
         $(this).addClass('active');
-        id = $('.page-content').children($(this).attr('href'));
+        var id = $('.page-content').children($(this).attr('href'));
         id.fadeIn().siblings('.hentry').hide();
+        if(history.pushState) {
+            history.pushState(null, null, '#'+id.attr('id'));
+        }
+        else {
+            location.hash = '#'+id.attr('id');
+        }
         return false;
     });
 
-    // show 1st article on load
-    $('.page-content .hentry').first().show();
-
+    // show article on load
+    if(window.location.hash) {
+        var urlID = $(window.location.hash);
+        var target = $("a[href='" + urlID.selector + "']");
+        target.addClass('active');
+        target.closest('.sub-menu').slideDown();
+        target.parents('li').addClass('active');
+        target.fadeIn().siblings('.hentry').hide();
+        console.log( '.' +urlID.attr('id') );
+    } else {
+        $('.page-content .hentry').first().show();
+    }
 });
